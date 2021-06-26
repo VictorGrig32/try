13cd6316d0e3db5c016cdcdfaf82571ffb9c40eb
Victor <80606722+VictorGrig32@users.noreply.github.com>
2021-06-27 00:12:28 +0300

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=a0525177_ved',
+    'username' => 'a0525177_ved',
+    'password' => 'banban6',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
